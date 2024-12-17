103e5c6a5db27ab68d4d81b89491631e560e5a62
Jinyoung Choi <jinyoung5497@gmail.com>
2024-12-18 00:48:25 +0900

    feat: custom button, constants created

--- frontend/src/screens/auth/AuthHomeScreen.tsx
+++ frontend/src/screens/auth/AuthHomeScreen.tsx
@@ -2,7 +2,8 @@ import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
 import {Button, View, SafeAreaView} from 'react-native';
 import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
-import {authNavigations} from '../../constants';
+import {authNavigations} from '../../constants/navigations';
+import CustomButton from '../../components/CustomButton';
 
 type AuthHomeScreenProps = StackScreenProps<AuthStackParamList>;
 
@@ -10,12 +11,14 @@ export default function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   return (
     <SafeAreaView>
       <View>
-        <Button
-          title="로그인 화면으로 이동"
+        <CustomButton
+          label="로그인하기"
+          variant="filled"
           onPress={() => navigation.navigate(authNavigations.LOGIN)}
         />
-        <Button
-          title="회원가입 화면으로 이동"
+        <CustomButton
+          label="회원가입하기"
+          variant="outlined"
           onPress={() => navigation.navigate(authNavigations.SIGNUP)}
         />
       </View>
