a8c0323c6ce2e752a9a6f2d9811a4ca76ca9826b
Jinyoung Choi <jinyoung5497@gmail.com>
2024-12-17 20:15:02 +0900

    chore: first commit

---
+++ frontend/App.tsx
@@ -0,0 +1,13 @@
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>텍스트</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
