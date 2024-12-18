0b8252f40787306625c9b564ce103f5fddbc1d83
Jinyoung Choi <jinyoung5497@gmail.com>
2024-12-18 11:48:17 +0900

    feat: custom input field / extra colors / custom inputfield implemented in login screen

--- frontend/src/screens/auth/LoginScreen.tsx
+++ frontend/src/screens/auth/LoginScreen.tsx
@@ -1,10 +1,63 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import InputField from '../../components/InputField';
 
 export default function LoginScreen() {
+  const [value, setValue] = useState({
+    email: '',
+    password: '',
+  });
+  const [touched, setTouched] = useState({
+    email: false,
+    password: false,
+  });
+
+  const handleChangeValue = (name: string, text: string) => {
+    setValue({
+      ...value,
+      [name]: text,
+    });
+  };
+
+  const handleBlur = (name: string) => {
+    setTouched({
+      ...touched,
+      [name]: true,
+    });
+  };
+
   return (
-    <View>
-      <Text>로그인 스크린</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="이메일"
+          error="이메일을 입력하세요"
+          inputMode="email"
+          value={value.email}
+          onChangeText={text => handleChangeValue('email', text)}
+          touched={touched.email}
+          onBlur={() => handleBlur('email')}
+        />
+        <InputField
+          placeholder="비밀번호"
+          error="비밀번호를 입력하세요"
+          secureTextEntry
+          value={value.password}
+          onChangeText={text => handleChangeValue('password', text)}
+          touched={touched.password}
+          onBlur={() => handleBlur('password')}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+  },
+});
