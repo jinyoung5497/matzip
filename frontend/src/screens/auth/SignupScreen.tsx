def1ca11039dd75fef4afa2178b82d2cfe06e8f8
Jinyoung Choi <jinyoung5497@gmail.com>
2024-12-19 19:39:11 +0900

    feat: signup screen / mergeRef custom function created / react-native-reanimated, react-native-safe-area-context version downgrade

--- frontend/src/screens/auth/SignupScreen.tsx
+++ frontend/src/screens/auth/SignupScreen.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React, {useRef} from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
 import InputField from '../../components/InputField';
 import {useForm} from '../../hooks/useForm';
 import CustomButton from '../../components/CustomButton';
 import {validateSignup} from '../../utils';
+import {TextInput} from 'react-native-gesture-handler';
 
 function SignupScreen() {
+  const passwordRef = useRef<TextInput | null>(null);
+  const passwordConfirmRef = useRef<TextInput | null>(null);
   const signup = useForm({
     initialValue: {email: '', password: '', passwordConfirm: ''},
     validate: validateSignup,
@@ -19,16 +22,25 @@ function SignupScreen() {
           error={signup.errors.email}
           touched={signup.touched.email}
           inputMode="email"
+          returnKeyType="next"
+          blurOnSubmit={false}
+          onSubmitEditing={() => passwordRef.current?.focus()}
           {...signup.getTextInputProps('email')}
         />
         <InputField
+          ref={passwordRef}
           placeholder="비밀번호"
+          textContentType="oneTimeCode"
           error={signup.errors.password}
           touched={signup.touched.password}
           secureTextEntry
+          returnKeyType="next"
+          blurOnSubmit={false}
+          onSubmitEditing={() => passwordConfirmRef.current?.focus()}
           {...signup.getTextInputProps('password')}
         />
         <InputField
+          ref={passwordConfirmRef}
           placeholder="비밀번호 확인"
           error={signup.errors.passwordConfirm}
           touched={signup.touched.passwordConfirm}
