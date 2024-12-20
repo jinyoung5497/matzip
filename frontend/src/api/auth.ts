1bbc986cb19a057d9d151d52fcbac2e55d228112
Jinyoung Choi <jinyoung5497@gmail.com>
2024-12-20 21:11:05 +0900

    feat: auth api established

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,60 @@
+import axiosInstance from './axios';
+import {Category, Profile} from '../types/domain';
+import {getEncryptStorage} from '../utils';
+
+export type RequestUser = {
+  email: string;
+  password: string;
+};
+
+export const postSignup = async ({
+  email,
+  password,
+}: RequestUser): Promise<void> => {
+  const {data} = await axiosInstance.post('/auth/signup', {
+    email,
+    password,
+  });
+
+  return data;
+};
+
+export type ResponseToken = {
+  accessToken: string;
+  refreshToken: string;
+};
+
+export const postLogin = async ({
+  email,
+  password,
+}: RequestUser): Promise<ResponseToken> => {
+  const {data} = await axiosInstance.post('/auth/signin', {
+    email,
+    password,
+  });
+
+  return data;
+};
+
+export type ResponseProfile = Profile & Category;
+
+export const getProfile = async (): Promise<ResponseProfile> => {
+  const {data} = await axiosInstance.get('/auth/me');
+
+  return data;
+};
+
+export const getAccessToken = async (): Promise<ResponseProfile> => {
+  const refreshToken = await getEncryptStorage('refreshToken');
+  const {data} = await axiosInstance.get('/auth/refresh', {
+    headers: {
+      Authorization: `Bearer ${refreshToken}`,
+    },
+  });
+
+  return data;
+};
+
+export const logout = async () => {
+  await axiosInstance.post('/auth/logout');
+};
